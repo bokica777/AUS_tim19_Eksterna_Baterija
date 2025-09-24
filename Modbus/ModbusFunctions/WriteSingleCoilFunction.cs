8aa28622eb7bfffda70d7bb183ffb054e3ba1c8b
Relja Opacic <opacicrelja@gmail.com>
2025-09-24 17:04:47 +0200

    ModbusFunctions Write

--- Modbus/ModbusFunctions/WriteSingleCoilFunction.cs
+++ Modbus/ModbusFunctions/WriteSingleCoilFunction.cs
@@ -9,6 +9,7 @@ namespace Modbus.ModbusFunctions
 {
     /// <summary>
     /// Class containing logic for parsing and packing modbus write coil functions/requests.
+    /// Upisivanje pojedinacnog digitalnog signala
     /// </summary>
     public class WriteSingleCoilFunction : ModbusFunction
     {
@@ -24,15 +25,40 @@ namespace Modbus.ModbusFunctions
         /// <inheritdoc />
         public override byte[] PackRequest()
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            byte[] req = new byte[12];
+
+            req[0] = BitConverter.GetBytes(CommandParameters.TransactionId)[1];
+            req[1] = BitConverter.GetBytes(CommandParameters.TransactionId)[0];
+            req[2] = BitConverter.GetBytes(CommandParameters.ProtocolId)[1];
+            req[3] = BitConverter.GetBytes(CommandParameters.ProtocolId)[0];
+            req[4] = BitConverter.GetBytes(CommandParameters.Length)[1];
+            req[5] = BitConverter.GetBytes(CommandParameters.Length)[0];
+            req[6] = CommandParameters.UnitId;
+            req[7] = CommandParameters.FunctionCode;
+            req[8] = BitConverter.GetBytes(((ModbusWriteCommandParameters)CommandParameters).OutputAddress)[1];
+            req[9] = BitConverter.GetBytes(((ModbusWriteCommandParameters)CommandParameters).OutputAddress)[0];
+            req[10] = BitConverter.GetBytes(((ModbusWriteCommandParameters)CommandParameters).Value)[1];
+            req[11] = BitConverter.GetBytes(((ModbusWriteCommandParameters)CommandParameters).Value)[0];
+
+            return req;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            Dictionary<Tuple<PointType, ushort>, ushort> resp = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            // adresa upisanpg signala iz odgovora se nalazi na poziciji 8 u response nizu
+            // adresa je 2 bajta, osma i deveta poz
+            // vrednost se nalazi na poziciji 10 u odgovoru
+            // vrednost takodje 2 bajta 
+            ushort outputAddress = (ushort)IPAddress.NetworkToHostOrder((short)BitConverter.ToUInt16(response, 8));
+            ushort value = (ushort)IPAddress.NetworkToHostOrder((short)BitConverter.ToUInt16(response, 10));
+
+            Tuple<PointType, ushort> tuple = new Tuple<PointType, ushort>(PointType.DIGITAL_OUTPUT, outputAddress);
+            resp.Add(tuple, value);
+
+            return resp;
         }
     }
 }
